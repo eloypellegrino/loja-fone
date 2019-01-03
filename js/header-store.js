30e66db3238583f577f08b22df47870fe5c122bc
eloypellegrino <eloypellegrino@hotmail.com>
2019-01-03 18:45:00 -0200

    Finaliza a aula 018 e o desafio

--- js/header-store.js
+++ js/header-store.js
@@ -2,18 +2,29 @@ const $search = document.querySelector("[type=search]");
 const $searchBtn = document.querySelector(".fa-search");
 const $searchBtnClose = document.querySelector(".fa-times");
 
-$searchBtn.addEventListener("click", () => {
+function searchClick() {
     $search.classList.toggle("-active");
     $searchBtn.classList.toggle("-active");
     $searchBtnClose.classList.toggle("-active");
     $searchBtn.classList.toggle("-hidden");
     $searchBtnClose.classList.toggle("-hidden");
-});
+};
 
-$searchBtnClose.addEventListener("click", () => {
-    $search.classList.toggle("-active");
-    $searchBtn.classList.toggle("-active");
-    $searchBtnClose.classList.toggle("-active");
-    $searchBtn.classList.toggle("-hidden");
-    $searchBtnClose.classList.toggle("-hidden");
-});
+$searchBtn.addEventListener("click", searchClick);
+$searchBtnClose.addEventListener("click", searchClick);
+
+const $mobileMenu = document.querySelector(".mobileMenu");
+const $navMenu = document.querySelectorAll(".header-store .action");
+
+function openMenu() {
+    $navMenu.forEach(element => {
+        if (element.style.display == "none") {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+    })
+
+}
+
+$mobileMenu.addEventListener("click", openMenu);
